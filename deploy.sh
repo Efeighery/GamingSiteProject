74a1ecbcc6ebdf7564ce2dcac227c79692176a00
Efeighery <56543538+Efeighery@users.noreply.github.com>
2023-04-03 21:03:00 +0100

    Added comments - Eoghan Feighery

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
+# Installing Nodejs NPM and update parameters
 sudo apt update && sudo apt install nodejs npm
 
+# Second npm installation
 sudo npm install -g pm2
 
+# For exiting Ubuntu server
 pm2 stop ex_app
 
+# Entering newly cloned Git repository
 cd GamingSiteProject/
 
+# NPM installation in repository directory
 npm install
 
+# Activating instance and triggering port tables
 pm2 start ./bin/www --name ex_app
